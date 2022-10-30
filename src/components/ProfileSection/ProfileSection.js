67a316e50b68d357d5efbfecd981b4ba75e51254
skyv26 <dev.aakashv@gmail.com>
2022-10-30 16:52:09 +0530

    Add the profile pic

--- src/components/ProfileSection/ProfileSection.js
+++ src/components/ProfileSection/ProfileSection.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import style from './ProfileSection.module.css';
+import ProfileImage from '../../assets/skyv.jpg';
 
 const ProfileSection = () => {
   return (
     <div>
-      
+      <img id="profile__img" class={style.Picture} src={ProfileImage} alt="See Aakash Verma" />
+
     </div>
   );
 };
