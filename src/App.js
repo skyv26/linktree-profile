807cf7cb7d2eaf35a6926892df1b32d7282a552f
skyv26 <dev.aakashv@gmail.com>
2022-10-30 17:04:09 +0530

    Add the `google-font` and `css-resetter`

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProfileSection from './components/ProfileSection/ProfileSection';
 
 const App = props => {
   return (
     <div>
-      
+      <ProfileSection />
     </div>
   );
 };
