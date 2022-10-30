a29522a16c544e5e9ebfdb8c8b3bd75ef98e2dbd
skyv26 <dev.aakashv@gmail.com>
2022-10-30 19:10:05 +0530

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import style from './Footer.module.css';
 import Zuri from '../../assets/zuri.png';
+import Dot from '../../assets/Vector.png';
 import I4G from '../../assets/I4G.png';
 
 const Footer = props => {
   return (
-    <footer>
+    <footer className={style.Footer}>
       <hr className={style.Line} />
       <a href="https://github.com/skyv26" className={style.Link} rel="noreferrer noopenner" target="_blank">
         <img src={Zuri} className={style.Image} alt="Let's connect on slack" />
+        <img src={Dot} className={`${style.Dot}`} alt="" />
       </a>
       <p className={style.Text}>HNG Internship 9 Frontend Task</p>
       <a href="https://github.com/skyv26" className={style.Link} rel="noreferrer noopenner" target="_blank">
