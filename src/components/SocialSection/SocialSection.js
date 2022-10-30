a4e47a6c80d43c030280efdfd53405cf2bccaea7
skyv26 <dev.aakashv@gmail.com>
2022-10-30 18:58:14 +0530

    Complete the mobile design

--- src/components/SocialSection/SocialSection.js
+++ src/components/SocialSection/SocialSection.js
@@ -5,20 +5,18 @@ import GithubImage from '../../assets/github.png';
 
 const SocialSection = props => {
   return (
-    <div className={style.SocialSection}>
-      <ul className={style.SocialUl}>
-        <li className={style.List}>
-          <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">
-            <img src={SlackImage} className={style.Image} alt="Let's connect on slack" />
-          </a>
-        </li>
-        <li className={style.List}>
-          <a href="https://github.com/skyv26" className={style.Link} rel="noreferrer noopenner" target="_blank">
-            <img src={GithubImage} className={style.Image} alt="Let's connect on slack" />
-          </a>
-        </li>
-      </ul>
-    </div>
+    <ul className={style.SocialUl}>
+      <li className={style.List}>
+        <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">
+          <img src={SlackImage} className={style.Image} alt="Let's connect on slack" />
+        </a>
+      </li>
+      <li className={style.List}>
+        <a href="https://github.com/skyv26" className={style.Link} rel="noreferrer noopenner" target="_blank">
+          <img src={GithubImage} className={style.Image} alt="Let's connect on slack" />
+        </a>
+      </li>
+    </ul>
   );
 };
 
