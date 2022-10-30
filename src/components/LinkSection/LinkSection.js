1a4e80dd654fdedf38479dce35a78c3aa7752e46
skyv26 <dev.aakashv@gmail.com>
2022-10-30 18:31:36 +0530

    Added the content in `LinkSection`

--- src/components/LinkSection/LinkSection.js
+++ src/components/LinkSection/LinkSection.js
@@ -5,22 +5,22 @@ const LinkSection = props => {
   return (
     <ul className={style.LinkSection}>
       <li className={style.List}>
-        <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">Twitter Link</a>
+        <a href="https://twitter.com/vrma_aakash" id="twitter" className={style.Link} rel="noreferrer noopenner" target="_blank">Twitter Link</a>
       </li>
       <li className={style.List}>
-        <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">Zuri Team</a>
+        <a href="https://training.zuri.team/" id="btn__zuri" className={style.Link} rel="noreferrer noopenner" target="_blank">Zuri Team</a>
       </li>
       <li className={style.List}>
-        <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">Zuri Books</a>
+        <a href="http://books.zuri.team/" id="books" className={style.Link} rel="noreferrer noopenner" target="_blank">Zuri Books</a>
       </li>
       <li className={style.List}>
-        <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">Python Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=skyv26" id="book__python" className={style.Link} rel="noreferrer noopenner" target="_blank">Python Books</a>
       </li>
       <li className={style.List}>
-        <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">Background Check for Coders</a>
+        <a href="https://background.zuri.team/" id="pitch" className={style.Link} rel="noreferrer noopenner" target="_blank">Let's Check Coders Experience</a>
       </li>
       <li className={style.List}>
-        <a href="." className={style.Link} rel="noreferrer noopenner" target="_blank">Design Books</a>
+        <a href="https://books.zuri.team/design-rules" className={style.Link} id="book__design" rel="noreferrer noopenner" target="_blank">Design Books</a>
       </li>
     </ul>
   );
