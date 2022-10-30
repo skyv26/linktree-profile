2a1cf5f8e9f500324419db1867543655f080aabd
skyv26 <dev.aakashv@gmail.com>
2022-10-30 16:58:56 +0530

    Remove default content from `[App.js]`

--- src/components/ProfileSection/ProfileSection.js
+++ src/components/ProfileSection/ProfileSection.js
@@ -4,7 +4,7 @@ import ProfileImage from '../../assets/skyv.jpg';
 
 const ProfileSection = () => {
   return (
-    <div>
+    <div class={style.ProfileContainer}>
       <img id="profile__img" class={style.Picture} src={ProfileImage} alt="See Aakash Verma" />
       <a href="https://twitter.com/vrma_aakash" target="_blank" rel="noreferrer noopenner" id="twitter" class={style.ProfileLink}>Aakash Verma</a>
       <a href="." target="_blank" rel="noreferrer noopenner" id="slack" class={`${style.ProfileLink} ${style.ShouldNotVisible}`}>skyv26</a>
