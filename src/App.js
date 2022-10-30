6a8c4adf21a3f31bb61588eb1c2aa5986483774e
skyv26 <dev.aakashv@gmail.com>
2022-10-30 19:10:30 +0530

    Update the folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ProfileSection from './components/ProfileSection/ProfileSection';
+import LinkSection from './components/LinkSection/LinkSection';
+import SocialSection from './components/SocialSection/SocialSection';
+import Footer from './components/Footer/Footer';
 
 const App = props => {
   return (
     <div>
       <ProfileSection />
+      <LinkSection />
+      <SocialSection />
+      <Footer />
     </div>
   );
 };
