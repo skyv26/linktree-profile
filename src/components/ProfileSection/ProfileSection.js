70593a4870be55d9ca54867ccdbbb04dbf005a2a
skyv26 <dev.aakashv@gmail.com>
2022-10-30 16:57:09 +0530

    add the content for profile section

--- src/components/ProfileSection/ProfileSection.js
+++ src/components/ProfileSection/ProfileSection.js
@@ -6,7 +6,8 @@ const ProfileSection = () => {
   return (
     <div>
       <img id="profile__img" class={style.Picture} src={ProfileImage} alt="See Aakash Verma" />
-
+      <a href="https://twitter.com/vrma_aakash" target="_blank" rel="noreferrer noopenner" id="twitter" class={style.ProfileLink}>Aakash Verma</a>
+      <a href="." target="_blank" rel="noreferrer noopenner" id="slack" class={`${style.ProfileLink} ${style.ShouldNotVisible}`}>skyv26</a>
     </div>
   );
 };
