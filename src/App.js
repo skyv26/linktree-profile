75abec692d120272db9426f2fd463f4c95560ab0
skyv26 <dev.aakashv@gmail.com>
2022-11-05 14:18:35 +0530

    `refactor:` add the routes and uncomment the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import ProfileSection from './components/ProfileSection/ProfileSection';
 import LinkSection from './components/LinkSection/LinkSection';
 import SocialSection from './components/SocialSection/SocialSection';
@@ -8,9 +9,9 @@ import Contact from './pages/Contact/Contact';
 const App = props => {
   return (
     <div>
-      {/* <ProfileSection /> */}
-      {/* <LinkSection />
-      <SocialSection /> */}
+      <ProfileSection />
+      <LinkSection />
+      <SocialSection />
       <Contact />
       <Footer />
     </div>
