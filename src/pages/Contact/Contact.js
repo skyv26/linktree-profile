ba14782bb5d2fd98587bfaedba9ad6a977eaaa2e
skyv26 <dev.aakashv@gmail.com>
2022-11-05 13:36:43 +0530

    `style:` update stylesheet for `contact` by adding more classes

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -10,18 +10,38 @@ const Contact = props => {
       </div>
       <form className='form'>
         
-        <label htmlFor="first_name" className="form-label">
-          First Name
-          <input type="text" id='first_name' placeholder="Enter your first name" className="form-input" />
-          <p className="input-msg first_name">Please enter your first name</p>
+        <div className="full-name--container">
+          <label htmlFor="first_name" className="form-label">
+            First Name
+            <input type="text" id="first_name" placeholder="Enter your first name" className="form-input" />
+            <p className="input-msg first_name">Please enter your first name</p>
+          </label>
+
+          <label htmlFor="last_name" className="form-label">
+            Last Name
+            <input type="text" id="last_name" placeholder="Enter your last name" className="form-input" />
+            <p className="input-msg last_name">Please enter your last name</p>
+          </label>
+        </div>
+
+        <label htmlFor="email" className="form-label">
+          Email
+          <input type="text" id="email" placeholder="yourname@email.com" className="form-input" />
+          <p className="input-msg email">Please enter your email</p>
         </label>
 
-        <label htmlFor="last_name" className="form-label">
-          Last Name
-          <input type="text" id='last_name' placeholder="Enter your last name" className="form-input" />
-          <p className="input-msg last_name">Please enter your last name</p>
+        <label htmlFor="message" className="form-label">
+          Message
+          <textarea id="message" placeholder="Send me a message and I'll reply you as soon as possible..." rows="4" className="form-input" />
+          <p className="input-msg message">Please enter a message</p>
         </label>
 
+        <label htmlFor="check" className="form-label checkbox-label">
+          <input type="checkbox" id="check" className="form-input form-checkbox--input" />
+          You agree to providing your data to Aakash who may contact you.
+        </label>
+
+        <button className="form-btn" id="btn__submit" type="submit">Send message</button>
       </form>
     </div>
   );
