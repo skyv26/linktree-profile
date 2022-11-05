b20a0f85457b9dc38769bdb2f0be11d639d487e3
skyv26 <dev.aakashv@gmail.com>
2022-11-05 12:33:34 +0530

    `feat:` add new page for contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = props => {
   return (
-    <div>
-      
+    <div className='contact-form--container'>
+      <div className="contact-form--info">
+        <h1 className="contact-form--info_title">Contact Me</h1>
+        <p className="contact-form--info_desc">Hi there, contact me to ask me about anything you have in mind.</p>
+      </div>
+      <form className='form'>
+        <label htmlFor="first_name" className="form-label">
+          First Name
+          <input type="text" id='first_name' placeholder="Enter your first name" className="form-input" />
+          <p className="input-msg first_name">Please enter a message</p>
+        </label>
+      </form>
     </div>
   );
 };
